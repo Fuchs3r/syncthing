0ebac148e1e7ebcc55591c7f638e291adcefc692
Fuchs3r <konsih@gmail.com>
2018-09-11 09:45:35 +0200

    #1

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if (( $EUID != 0 )); then
-    echo "Please run as root"
-    exit
-else
-
 usr=$(whoami)
 
 # Add the release PGP keys:
@@ -15,16 +10,14 @@ echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sourc
 
 # Update and install syncthing:
 sudo apt-get update
-sudo apt-get install apache2 syncthing -y
-clear
+sudo apt-get install nano net-tools apache2 syncthing -y
 
 # enable syncthing as service and starts it for the first time
 systemctl enable syncthing@$usr.service
 systemctl start syncthing@$usr.service
-clear
 
 echo "[..] -- please wait ..."
-sleep 45s
+sleep 30s
 clear
 
 echo "[..] -- syncthing is up and running"
@@ -38,7 +31,7 @@ clear
 echo "[..] -- the config file will now opend up"
 echo "[..] -- now you have to update your ip for the webinterface"
 echo "[..] -- your ip is ..."
-ifconfig
+
 
 sleep 10s
 read -p "[..] -- press enter to continue"
@@ -49,5 +42,3 @@ echo "[..] -- syncthing is up and running. Enjoy!"
 read -p "[..] -- press enter to end"
 clear
 exit
-
-fi
